d9f9e9287d8c6576bc932652985b41d116d60453
Jonathan Edwards <jonathae@cisco.com>
2023-12-07 07:53:46 -0800

    07.2

--- day07/day07.cpp
+++ day07/day07.cpp
@@ -9,16 +9,32 @@
 #include "include/getdata.h"
 #include "include/stringstuff.h"
 
-enum class Type
+
+
+struct Hand
 {
-    HighCard,
-    Pair,
-    TwoPair,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,        
+    enum class Type
+    {
+        HighCard,
+        Pair,
+        TwoPair,
+        ThreeOfAKind,
+        FullHouse,
+        FourOfAKind,
+        FiveOfAKind,        
+    };
+
+
+    Type                type;
+    std::array<int,5>   cards;
+    int                 bid;
+
+    friend auto operator<=>(Hand const&,Hand const&)=default;
+
+
 };
+
+
 
 
 
@@ -27,115 +43,228 @@ namespace Part1
 
 std::map<char,int>  faceValues
 {
-    {'2',2},
-    {'3',3},
-    {'4',4},
-    {'5',5},
-    {'6',6},
-    {'7',7},
-    {'8',8},
-    {'9',9},
-    {'T',10},
-    {'J',11},
-    {'Q',12},
-    {'K',13},
-    {'A',14},
+    {'2',2},    {'3',3},    {'4',4},    {'5',5},    {'6',6},    {'7',7},    {'8',8},    
+    {'9',9},    {'T',10},   {'J',11},   {'Q',12},   {'K',13},    {'A',14},
 };
 
 
 
-auto classify(std::string_view view)
+auto classify(std::string cards)
 {
-    int             counts[6]{};
+    std::ranges::sort(cards);
 
-    std::string     s{view};
-    char            previous{};
-    int             count{}; 
+    int             inARow[6]{};
 
-    std::ranges::sort(s);
+    int             count{1}; 
+    char            previous{cards[0]};
 
-    for(auto c:s)
+    for(char card : std::ranges::drop_view{cards, 1})
     {
-        if(c == previous)
+        if(card == previous)
         {
             count++;
         }
         else
         {
-            counts[count]++;
+            inARow[count]++;
             count=1;
         }
-        previous=c;
+        previous=card;
     }
-    counts[count]++;
+    inARow[count]++;
 
-    if(counts[5])
+// ---
+
+    if(inARow[5])
     {
-        return Type::FiveOfAKind;
+        return Hand::Type::FiveOfAKind;
     }
-    else if(counts[4])
+    else if(inARow[4])
     {
-        return Type::FourOfAKind;
+        return Hand::Type::FourOfAKind;
     }
-    else if(counts[3] && counts[2])
+    else if(inARow[3] && inARow[2])
     {
-        return Type::FullHouse;
+        return Hand::Type::FullHouse;
     }
-    else if(counts[3])
+    else if(inARow[3])
     {
-        return Type::ThreeOfAKind;
+        return Hand::Type::ThreeOfAKind;
     }
-    else if(counts[2] == 2)
+    else if(inARow[2] == 2)
     {
-        return Type::TwoPair;
+        return Hand::Type::TwoPair;
     }
-    else if(counts[2] == 1)
+    else if(inARow[2] == 1)
     {
-        return Type::Pair;
+        return Hand::Type::Pair;
     }
     else
     {
-        return Type::HighCard;
+        return Hand::Type::HighCard;
     }
 }
 
 
-
-
-struct Hand
+Hand  makeHand(std::string_view  cards, int bid) 
 {
-    Type                type;
-    std::array<int,5>   cards;
-    int                 bid;
+    assert(cards.size()==5);
 
-    friend auto operator<=>(Hand const&,Hand const&)=default;
+    Hand    hand{classify(std::string{cards}), {}, bid};
+
+    std::ranges::transform(cards,hand.cards.begin(), [](char card){return faceValues[card];});
+
+    return hand;
+}
 
 
-    Hand(std::string_view  view, int bid) : bid{bid}, type{classify(view)}
-    {
-        assert(view.size()==5);
+}
 
-        std::ranges::transform(view,cards.begin(), [](char card){return faceValues[card];});
 
-    }
+namespace Part2
+{
+std::map<char,int>  faceValues
+{
+    {'J',1},    {'2',2},    {'3',3},    {'4',4},    {'5',5},    {'6',6},    {'7',7},
+    {'8',8},    {'9',9},    {'T',10},   {'Q',12},   {'K',13},   {'A',14},
 };
 
+
+auto classify(std::string cards)
+{
+    std::ranges::sort(cards);
+
+    auto            jokerCount = std::ranges::count(cards,'J');
+
+    int             inARow[6]{};
+
+    int             count{1}; 
+    char            previous{cards[0]};
+
+    for(char card : std::ranges::drop_view{cards, 1})
+    {
+        if(card == previous)
+        {
+            count++;
+        }
+        else
+        {
+            inARow[count]++;
+            count=1;
+        }
+        previous=card;
+    }
+    inARow[count]++;
+
+// ---
+
+    if(inARow[5])
+    {
+        // irrelevent whether they're jokers or not.
+        return Hand::Type::FiveOfAKind;
+    }
+    else if(inARow[4])
+    {
+        if(jokerCount)                  // either 1 or 4.       JJJJX -> XXXXX <- JXXXX
+        {
+            assert(jokerCount == 1 || jokerCount == 4);
+            return Hand::Type::FiveOfAKind;
+        }
+        else
+        {
+            return Hand::Type::FourOfAKind;
+        }
+    }
+    else if(inARow[3] && inARow[2])    
+    {
+        if(jokerCount)                  // either 2 or 3.       JJJXX -> XXXXX < JJXXX
+        {
+            assert(jokerCount == 2 || jokerCount == 3);
+            return Hand::Type::FiveOfAKind;
+        }
+        else
+        {
+            return Hand::Type::FullHouse;
+        }
+    }
+    else if(inARow[3])
+    {
+        if(jokerCount)                  // either 3 or 1.       JXXXY -> XXXXY <- JJJXY
+        {
+            assert(jokerCount == 1 || jokerCount == 3);
+            return Hand::Type::FourOfAKind;
+        }
+        else
+        {
+            return Hand::Type::ThreeOfAKind;
+        }
+    }
+    else if(inARow[2] == 2)
+    {
+        if(jokerCount == 2)             //                      JJXXY -> XXXXY  
+        {
+            return Hand::Type::FourOfAKind;
+        }
+        else if(jokerCount == 1)        //                      JXXYY -> XXXYY  
+        {
+            return Hand::Type::FullHouse;
+        }
+        else
+        {
+            return Hand::Type::TwoPair;
+        }
+    }
+    else if(inARow[2] == 1)
+    {
+        if(jokerCount)                  //                      JJXYZ -> XXXYZ <  JXXYZ     
+        {
+            return Hand::Type::ThreeOfAKind;
+        }
+        else
+        {
+            return Hand::Type::Pair;
+        }
+    }
+    else
+    {
+        if(jokerCount)                  //                      JWXYZ -> WWXYZ
+        {
+            return Hand::Type::Pair;
+        }
+        else
+        {
+            return Hand::Type::HighCard;
+
+        }
+    }
 }
 
 
+Hand  makeHand(std::string_view  cards, int bid) 
+{
+    assert(cards.size()==5);
+
+    Hand    hand{classify(std::string{cards}), {}, bid};
+
+    std::ranges::transform(cards,hand.cards.begin(), [](char card){return faceValues[card];});
+
+    return hand;
+}
+
+}
 
 int main()
 try
 {
-    std::vector<Part1::Hand>       hands1;
-    std::vector<Part1::Hand>       hands2;
+    std::vector<Hand>       hands1;
+    std::vector<Hand>       hands2;
 
     for(auto const &line : getDataLines())
     {
         auto [cards,bid] = splitIn2(line,' ');  //266QJ 288
 
-        hands1.emplace_back(cards, stoi(bid));
-        hands2.emplace_back(cards, stoi(bid));
+        hands1.push_back(Part1::makeHand(cards, stoi(bid)));
+        hands2.push_back(Part2::makeHand(cards, stoi(bid)));
     }
 
     std::ranges::sort(hands1);
