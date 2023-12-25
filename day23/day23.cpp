1b913951df97522966eeb5b435287beff6caedd9
Jon <jon@jonstuff.com>
2023-12-24 18:06:48 -0800

    23.0

---
+++ day23/day23.cpp
@@ -0,0 +1,27 @@
+#include <print>
+
+#include "include/codeAnalysis.h"
+#include "include/thrower.h"
+#include "include/getdata.h"
+
+
+
+int main()
+try
+{
+    for(auto const &line : getDataLines())
+    {
+    }
+}
+catch(std::exception const &e)
+{
+    std::print("{}",e.what());
+}
+
+
+#include <sstream>
+
+// --------------------------
+std::istringstream testInput{
+R"(
+)"};
